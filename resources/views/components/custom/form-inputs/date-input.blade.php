ef3be424922474cb5bb0fdbd31c3eca24f9d8bf1
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-05-30 00:34:52 +0000

    fix: Correct date format in date input component and adjust label alignment

--- resources/views/components/custom/form-inputs/date-input.blade.php
+++ resources/views/components/custom/form-inputs/date-input.blade.php
@@ -13,10 +13,14 @@
     <label class="block text-sm text-dark font-medium mb-1 dark:text-white"">{{ $label }}</label>
     <div class=" relative max-w-sm">
 
-            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <div class="absolute inset-y-0 inset-s-0 flex items-center ps-3 pointer-events-none">
                 <i class="fi fi-rr-calendar" width="24" height="24"></i>
             </div>
-            <input datepicker datepicker-autohide datepicker-format="dd-mm-yyyy" datepicker-autoselect-today
+            <input
+                datepicker
+                datepicker-autohide
+                datepicker-format="yyyy-mm-dd"
+                datepicker-autoselect-today
                 name="{{ $name }}" id="{{ $id }}" type="text"
                 class="block w-full ps-9 pe-3 py-2.5 form-control bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-1 focus:ring-brand focus:border-brand px-3  shadow-xs placeholder:text-body"
                 placeholder="{{ $placeholder }}" {{ $required ? 'required' : '' }}>
