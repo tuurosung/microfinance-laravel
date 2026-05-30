891a61e9ce83b51d509b73410cc483db60b743f1
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-05-30 00:35:04 +0000

    fix: Adjust formatting in head.blade.php and reposition Flowbite script inclusion

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,12 +2,14 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 <title>
-    {{ filled($title ?? null) ? $title.' - '.config('app.name', 'Laravel') : config('app.name', 'Laravel') }}
+    {{ filled($title ?? null) ? $title . ' - ' . config('app.name', 'Laravel') : config('app.name', 'Laravel') }}
 </title>
 
 <link rel="icon" href="/favicon.ico" sizes="any">
 <link rel="icon" href="/favicon.svg" type="image/svg+xml">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+<script src="https://cdn.jsdelivr.net/npm/flowbite@4.0.1/dist/flowbite.min.js"></script>
 
 @fonts
 
