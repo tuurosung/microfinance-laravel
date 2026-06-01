dd0cb5eeb06387c1dc5aed8143e1c2ddab31a50c
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-01 13:07:21 +0000

    feat: Update recreateCifTable command to include KYC migrations

--- app/Console/Commands/RecreateCifTable.php
+++ app/Console/Commands/RecreateCifTable.php
@@ -31,20 +31,39 @@ class RecreateCifTable extends Command
         // drop table
         Schema::dropIfExists('cifs');
         Schema::dropIfExists('kycs');
+        Schema::dropIfExists('kyc_aml');
+        Schema::dropIfExists('kyc_documents');
+        Schema::dropIfExists('kyc_ghana_cards');
 
 
         // get miration up function
         $cif_migration = database_path('migrations/2026_05_14_094950_create_cifs_table.php');
         $kyc_migration = database_path('migrations/2026_05_20_202009_create_kycs_table.php');
+        $kyc_aml_migration = database_path('migrations/2026_05_30_181638_create_kyc_amls_table.php');
+        $kyc_documents_migration = database_path('migrations/2026_05_29_002443_create_kyc_documents_table.php');
+        $kyc_ghana_card_migration = database_path('migrations/2026_05_30_181813_create_kyc_ghana_cards_table.php');
 
-        $migration = require $cif_migration;
-        $migration->up();
+        $migrations = [
+            $cif_migration,
+            $kyc_migration,
+            $kyc_aml_migration,
+            $kyc_documents_migration
+        ];
 
-        // run kyc migration
-        $migration = require $kyc_migration;
-        $migration->up();
+        foreach ($migrations as $migration) {
+            $migration = require $migration;
+            $migration->up();
+        }
 
-        //
+
+        // $migration = require $cif_migration;
+        // $migration->up();
+
+        // // run kyc migration
+        // $migration = require $kyc_migration;
+        // $migration->up();
+
+        // //
 
         // set foreign key locks to 1
         Schema::enableForeignKeyConstraints();
