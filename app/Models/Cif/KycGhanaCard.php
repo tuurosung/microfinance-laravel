721d6b96c8f21540238425d4db67481c7a71dbcb
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-01 13:01:41 +0000

    feat: Add KycGhanaCard model for managing Ghana Card KYC data

---
+++ app/Models/Cif/KycGhanaCard.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models\Cif;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class KycGhanaCard extends Model
+{
+    protected $table = 'kyc_ghana_cards';
+    protected $primaryKey = 'id';
+    protected $keyType = 'integer';
+    public $incrementing = true;
+
+
+    protected $fillable =  [
+        'ghana_card_status',
+        'ghana_card_number'
+    ];
+
+
+    public function kyc(): BelongsTo
+    {
+        return $this->belongsTo(Kyc::class, 'kyc_id', 'kyc_id');
+    }
+}
