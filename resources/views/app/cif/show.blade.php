af21304a9444c6539e4eeaa182e034e7ea5a54eb
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-01 13:01:16 +0000

    feat: Enhance CIF show view with KYC verification details and improved tab labels

--- resources/views/app/cif/show.blade.php
+++ resources/views/app/cif/show.blade.php
@@ -2,7 +2,7 @@
 
     <div class="mx-auto max-w-7xl">
 
-        <x-custom.headers.page-header title="Cif#{{ $cif->cif_number }}" />
+        <x-custom.headers.page-header title="Cif#: {{ $cif->cif_number }}" currentPage="Cif File" />
 
 
         <div class="grid grid-cols-12 gap-8">
@@ -86,37 +86,208 @@
                         <li class="me-2" role="presentation">
                             <button class="inline-block p-4 border-b-2 rounded-t-base cursor-pointer" id="profile-tab"
                                 data-tabs-target="#profile" type="button" role="tab" aria-controls="profile"
-                                aria-selected="false">Profile</button>
+                                aria-selected="false">KYC Verification</button>
                         </li>
                         <li class="me-2" role="presentation">
                             <button
                                 class="inline-block p-4 border-b-2 rounded-t-base hover:text-fg-brand hover:border-brand"
                                 id="dashboard-tab" data-tabs-target="#dashboard" type="button" role="tab"
-                                aria-controls="dashboard" aria-selected="false">Dashboard</button>
+                                aria-controls="dashboard" aria-selected="false">Documents</button>
                         </li>
                         <li class="me-2" role="presentation">
                             <button
                                 class="inline-block p-4 border-b-2 rounded-t-base hover:text-fg-brand hover:border-brand"
                                 id="settings-tab" data-tabs-target="#settings" type="button" role="tab"
-                                aria-controls="settings" aria-selected="false">Settings</button>
+                                aria-controls="settings" aria-selected="false">Accounts</button>
                         </li>
                         <li role="presentation">
                             <button
                                 class="inline-block p-4 border-b-2 rounded-t-base hover:text-fg-brand hover:border-brand"
                                 id="contacts-tab" data-tabs-target="#contacts" type="button" role="tab"
-                                aria-controls="contacts" aria-selected="false">Contacts</button>
+                                aria-controls="contacts" aria-selected="false">Contact Information</button>
                         </li>
                     </ul>
                 </div>
                 <div id="default-tab-content">
                     <div class="hidden p-4 rounded-base bg-neutral-secondary-soft" id="profile" role="tabpanel"
                         aria-labelledby="profile-tab">
-                        <p class="text-sm text-body">This is some placeholder content the <strong
-                                class="font-medium text-heading">Profile tab's associated content</strong>. Clicking
-                            another tab will
-                            toggle the visibility of this one for the next. The tab JavaScript swaps classes to control
-                            the content
-                            visibility and styling.</p>
+
+                        @if (!$cif->kyc)
+                        <div class="flex items-start sm:items-center p-4 mb-4 text-sm text-fg-danger-strong rounded-2 bg-danger-softer border border-danger-subtle"
+                            role="alert">
+                            <i class="fi fi-rr-triangle-warning me-1"></i>
+                            <p>
+                                <span class="font-medium me-2">Attention!</span> KYC Information Not Found
+                                <a href="{{ route('cif.kyc.create', $cif) }}" class="underline">Click here</a> to add
+                            </p>
+                        </div>
+                        @endif
+
+                        <section class="mb-30">
+
+                            <div class="flex justify-between items-center mb-5">
+                                <h5 class="">Ghana Card</h5>
+                                <div>
+                                    <x-custom.ui-elements.dropdown.dropdown-parent label="Actions">
+                                        <x-custom.ui-elements.dropdown.dropdown-item label="Verified" />
+                                        <x-custom.ui-elements.dropdown.dropdown-item label="Rejected" />
+                                        <x-custom.ui-elements.dropdown.dropdown-item label="Fake / Forgery" />
+                                    </x-custom.ui-elements.dropdown.dropdown-parent>
+
+                                </div>
+                            </div>
+
+                            <table class="w-full text-sm text-left rtl:text-right text-black">
+                                <thead>
+                                    <tr class="border-y border-default">
+                                        <th scope="row" class="pe-6 py-2 font-medium text-gray-900 whitespace-nowrap">
+                                            Ghana Card Number
+                                        </th>
+                                        <th scope="row" class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap">
+                                            Verification Status
+                                        </th>
+                                        <th scope="row" class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap">
+                                            Date/Time
+                                        </th>
+                                        <th scope="row" class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap text-end">
+                                            Verification Officer
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr class="border-b border-default">
+                                        <td class="pe-6 py-2">
+                                            {{ $cif->kyc?->ghana_card_number }}
+                                        </td>
+                                        <td class="px-6 py-2">
+                                            Verified
+                                        </td>
+                                        <td class="px-6 py-2">
+                                            Time Verified
+                                        </td>
+                                        <td class="px-6 py-2 text-end">
+                                            Verified By
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+
+                        </section>
+
+
+                        <!-- Address -->
+                        <section class="mb-30">
+
+                            <div class="flex justify-between items-center mb-5">
+                                <h5 class="">Address</h5>
+                                <div>
+                                    <x-custom.ui-elements.dropdown.dropdown-parent label="Actions">
+                                        <x-custom.ui-elements.dropdown.dropdown-item label="Verified" />
+                                        <x-custom.ui-elements.dropdown.dropdown-item label="Rejected" />
+                                        <x-custom.ui-elements.dropdown.dropdown-item label="Fake / Forgery" />
+                                    </x-custom.ui-elements.dropdown.dropdown-parent>
+
+                                </div>
+                            </div>
+
+                            <table class="w-full text-sm text-left rtl:text-right text-black">
+                                <thead>
+                                    <tr class="border-y border-default">
+                                        <th scope="row" class="pe-6 py-2 font-medium text-gray-900 whitespace-nowrap">
+                                            GPS Address
+                                        </th>
+                                        <th scope="row" class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap">
+                                            Verification Status
+                                        </th>
+                                        <th scope="row" class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap">
+                                            Date/Time
+                                        </th>
+                                        <th scope="row" class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap text-end">
+                                            Verification Officer
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr class="border-b border-default">
+                                        <td class="pe-6 py-2">
+                                            {{ $cif->kyc?->digital_address }}
+                                        </td>
+                                        <td class="px-6 py-2">
+                                            Verified
+                                        </td>
+                                        <td class="px-6 py-2">
+                                            Time Verified
+                                        </td>
+                                        <td class="px-6 py-2 text-end">
+                                            Verified By
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+
+                        </section>
+
+
+                        <!-- Source Of Funds -->
+                        <section>
+
+                            <div class="flex justify-between items-center mb-5">
+                                <h5 class="">Source Of Funds</h5>
+                                <div>
+                                    <x-custom.ui-elements.dropdown.dropdown-parent label="Actions">
+                                        <x-custom.ui-elements.dropdown.dropdown-item label="Verified" />
+                                        <x-custom.ui-elements.dropdown.dropdown-item label="Rejected" />
+                                        <x-custom.ui-elements.dropdown.dropdown-item label="Fake / Forgery" />
+                                    </x-custom.ui-elements.dropdown.dropdown-parent>
+
+                                </div>
+                            </div>
+
+                            <table class="w-full text-sm text-left rtl:text-right text-black">
+                                <thead>
+                                    <tr class="border-y border-default">
+                                        <th scope="row" class="pe-6 py-2 font-medium text-gray-900 whitespace-nowrap">
+                                            Source Of Funds
+                                        </th>
+                                        <th scope="row" class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap">
+                                            Verification Status
+                                        </th>
+                                        <th scope="row" class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap">
+                                            Date/Time
+                                        </th>
+                                        <th scope="row" class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap text-end">
+                                            Verification Officer
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr class="border-b border-default">
+                                        <td class="pe-6 py-2">
+                                            {{ $cif->kyc?->source_of_funds->label() }}
+                                        </td>
+                                        <td class="px-6 py-2">
+                                            Verified
+                                        </td>
+                                        <td class="px-6 py-2">
+                                            Time Verified
+                                        </td>
+                                        <td class="px-6 py-2 text-end">
+                                            Verified By
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+
+                        </section>
+
+
+
+
+
+
+
+
+
                     </div>
                     <div class="hidden p-4 rounded-base bg-neutral-secondary-soft" id="dashboard" role="tabpanel"
                         aria-labelledby="dashboard-tab">
