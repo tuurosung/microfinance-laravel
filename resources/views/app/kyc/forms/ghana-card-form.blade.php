b5fe8465596e7e8303ce90a79084efda9f1662ea
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-02 12:00:05 +0000

    fix: Update field names for Ghana Card status and number in the form

--- resources/views/app/kyc/forms/ghana-card-form.blade.php
+++ resources/views/app/kyc/forms/ghana-card-form.blade.php
@@ -5,14 +5,14 @@
 <div class="grid grid-cols-12 gap-6 mb-5">
     <div class="lg:col-span-3 md:col-span-3 sm:col-span-12 col-span-12">
 
-        <x-custom.form-inputs.select label="Ghana Card Status" name="ghana_card_status" id="ghana_card_status"
-            :options="$ghanaCardStatus" :selected="$cif->kyc->ghanaCard->ghana_card_status ?? ''" required />
+        <x-custom.form-inputs.select label="Ghana Card Status" name="card_status" id="card_status"
+            :options="$ghanaCardStatus" :selected="$cif->kyc->ghanaCard->card_status ?? ''" required />
 
     </div>
     <div class="lg:col-span-3 md:col-span-6 sm:col-span-12 col-span-12">
 
-        <x-custom.form-inputs.text-input label="Ghana Card Number" name="ghana_card_number" id="ghana_card_number"
-            :value="$cif->kyc->ghanaCard->ghana_card_number ?? ''" placeholder="GHA-XXXXXXXX-X" required />
+        <x-custom.form-inputs.text-input label="Ghana Card Number" name="card_number" id="card_number"
+            :value="$cif->kyc->ghanaCard->card_number ?? ''" placeholder="GHA-XXXXXXXX-X" required />
 
     </div>
 </div>
