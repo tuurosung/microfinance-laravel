08628cc0001a9708028bcc723bac1cf435d692ef
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-02 09:06:53 +0000

    feat: Add user profile card with contact and address details for KYC compliance

---
+++ resources/views/components/custom/cif/profile.blade.php
@@ -0,0 +1,69 @@
+<div class="card sticky top-10">
+    <div class="card-header px-3 py-6 bg-blue-500 text-white rounded-t-xl">
+        <div
+            class="border-2 border-primary rounded-full h-[72px] w-[72px] flex justify-center items-center relative mx-auto mb-4">
+            <img src="{{ asset('user-1.jpg') }}" class="rounded-full h-[60px]" alt="user1">
+
+        </div>
+        <h3 class="text-white font-medium font-cal-sans-regular text-center text-lg">
+            Mathew Anderson
+        </h3>
+    </div>
+    <div class="card-body">
+
+        <div class="flex gap-4 mb-3">
+            <span>
+                <i class="fi fi-rr-phone-call"></i>
+            </span>
+            <span>
+                0213456789
+            </span>
+        </div>
+
+        <div class="flex gap-4 mb-3">
+            <span>
+                <i class="fi fi-rr-marker"></i>
+            </span>
+            <span>
+                123 Main Street, New York
+            </span>
+        </div>
+
+        <div class="flex gap-4 mb-3">
+            <span>
+                <i class="fi fi-rr-newsletter-subscribe"></i>
+            </span>
+            <span>
+                123 Main Street, New York
+            </span>
+        </div>
+
+    </div>
+</div>
+
+<div class="sr-only">
+
+    <ul class="max-w-xs flex flex-col font-inter-regular">
+        <li
+            class="inline-flex items-center gap-x-3.5 py-3 px-4 font-medium bg-white border border-gray-200 text-success-600 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg dark:bg-transparent dark:border-gray-700 dark:text-white font-cal-sans-regular text-lg">
+            KYC Compliance 1/3
+        </li>
+        <li class="inline-flex items-center gap-x-3.5 py-3 px-4 text-sm font-medium
+                                bg-white border border-gray-200 text-success-600 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg dark:bg-transparent
+                                dark:border-gray-700 dark:text-white">
+            <i class="fi fi-rr-check text-md leading-tight font-medium text-success"></i>
+            Ghana Card Verification
+        </li>
+        <li
+            class="inline-flex items-center gap-x-3.5 py-3 px-4 text-sm font-medium bg-white border border-gray-200 text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg dark:bg-transparent dark:border-gray-700 dark:text-white">
+            <i class="fi fi-rr-cross text-md leading-tight font-medium text-error"></i>
+            Digital Address Verification
+        </li>
+        <li
+            class="inline-flex items-center gap-x-3.5 py-3 px-4 text-sm font-medium bg-white border border-gray-200 text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg dark:bg-transparent dark:border-gray-700 dark:text-white">
+            <i class="fi fi-rr-cross text-md leading-tight font-medium text-error"></i>
+            AML Verification
+        </li>
+    </ul>
+
+</div>
