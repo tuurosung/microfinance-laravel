f6a4d060564a9d95980a1ad46ad9d1aa861094cf
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-02 09:18:22 +0000

    feat: Add KycAml model for managing AML data related to KYC processes

---
+++ app/Models/Kyc/KycAml.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models\Kyc;
+
+use App\Models\Cif\Kyc;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class KycAml extends Model
+{
+    protected $table = 'kyc_aml';
+    protected $primaryKey = 'id';
+    protected $keyType = 'integer';
+    public $incrementing = true;
+
+
+    protected $fillable = [
+        'source_of_funds',
+        'employment_status',
+        'occupation',
+        'employer_name',
+        'monthly_income',
+    ];
+
+    public function kyc(): BelongsTo
+    {
+        return $this->belongsTo(Kyc::class, 'kyc_id', 'kyc_id');
+    }
+}
