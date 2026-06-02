4d269e178df59a2fe8c534cfd81120238a2b0d7e
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-02 09:17:04 +0000

    feat: Create StoreCifRequest for validating CIF data submissions

---
+++ app/Http/Requests/Cif/StoreCifRequest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Requests\Cif\Cif;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCifRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => [
+                'required',
+                'string',
+                'max:255',
+            ],
+            'other_names' => [
+                'required',
+                'string',
+                'max:255',
+            ],
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'max:255',
+                'unique:users',
+            ],
+            'phone_number' => [
+                'required',
+                'string',
+                'max:255',
+            ],
+            'secondary_phone' => [
+                'required',
+                'string',
+                'max:255',
+            ],
+            'sex' => [
+                'required',
+                'string',
+                'max:255',
+            ],
+            'date_of_birth' => [
+                'required',
+                'date',
+            ],
+            'residential_address' => [
+                'required',
+                'string',
+                'max:255',
+            ],
+        ];
+    }
+}
