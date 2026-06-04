1098ff80933f4e92648d00d1f7bbd6379b99a02e
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-04 05:42:25 +0000

    feat: Add casts for source_of_funds and employment_status in KycAml model

--- app/Models/Kyc/KycAml.php
+++ app/Models/Kyc/KycAml.php
@@ -2,6 +2,8 @@
 
 namespace App\Models\Kyc;
 
+use App\Enums\Kyc\EmploymentStatusEnum;
+use App\Enums\Kyc\SourceOfFundsEnum;
 use App\Models\Kyc\Kyc;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -20,6 +22,12 @@ class KycAml extends Model
         'occupation',
         'employer_name',
         'monthly_income',
+    ];
+
+
+    protected $casts = [
+        'source_of_funds' => SourceOfFundsEnum::class,
+        'employment_status' => EmploymentStatusEnum::class,
     ];
 
     public function kyc(): BelongsTo
