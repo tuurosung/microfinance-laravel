e03780e74b711d79c87a1d765a09281c49534b74
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-06-23 06:39:38 +0000

    refactor: Update KYC submission component to utilize KycSubmissionService and improve method structure

--- resources/views/components/kyc/⚡submit-kyc-for-approval.blade.php
+++ resources/views/components/kyc/⚡submit-kyc-for-approval.blade.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Models\Cif\Cif;
-use App\Models\Cif\Kyc;
+use App\Domain\CIFs\Models\Cif;
+use App\Domain\KYC\Models\Kyc;
+use App\Domain\KYC\Services\KycSubmissionService;
 use App\Services\Kyc\SubmitKycForApproval;
 use Livewire\Component;
 
@@ -9,48 +10,32 @@ new class extends Component {
 
     public Cif $cif;
     public Kyc $kyc;
+    // public KycSubmissionService $submissionService;
 
-    public function submit()
+    public function boot(): void
     {
-        $submitKycForApproval = new SubmitKycForApproval();
+        $this->submissionService = new KycSubmissionService($this->cif, $this->kyc);
+    }
 
-        if ($submitKycForApproval->submitKYC($this->kyc, $this->cif)) {
 
-            // $this->modal('submit-kyc')->close();
-            return redirect()->route('cif.kyc.index');
+    public function mount(Cif $cif, Kyc $kyc) {
+        $this->cif = $cif;
+        $this->kyc = $kyc;
+    }
 
+    public function submitKycFrmForApproval()
+    {
+        try {
+            $this->submissionService->submit();
+            return redirect()->route('cif.show', [$this->cif]);
+        } catch (\Exception $e) {
+            // $this->addError($e->getMessage());
+            throw $e;
         }
     }
 };
 ?>
 
 <div>
-    <flux:modal.trigger name="submit-kyc">
 
-        <button class="btn-md flex items-center gap-2 border-0 bg-blue-600 text-white rounded cursor-pointer align-middle">
-            <i class="fi fi-br-check me-3"></i>
-            Submit KYC Form
-        </button>
-
-    </flux:modal.trigger>
-
-
-    <flux:modal name="submit-kyc" class="min-w-88" :dismissible="false">
-        <div class="space-y-6">
-            <div>
-                <flux:heading size="lg">Submit KYC Form?</flux:heading>
-                <flux:text class="mt-2">
-                    You're about to submit the KYC form.<br>
-                    This action cannot be reversed.
-                </flux:text>
-            </div>
-            <div class="flex gap-2">
-                <flux:spacer />
-                <flux:modal.close>
-                    <flux:button variant="ghost">Cancel</flux:button>
-                </flux:modal.close>
-                <flux:button type="submit" variant="primary" wire:click="submit">Submit KYC Form</flux:button>
-            </div>
-        </div>
-    </flux:modal>
 </div>
