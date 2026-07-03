75de670c1292d9f7b21554fca454b50de22bd59f
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-07-03 04:29:36 +0000

    feat: Create WithdrawalRequest model for managing withdrawal requests and their attributes

---
+++ app/Domain/Transactions/Models/WithdrawalRequest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare (strict_types= 1);
+
+namespace App\Domain\Transactions\Models;
+
+use App\Domain\Accounts\Models\Account;
+use App\Enums\Transactions\MomoProviderEnum;
+use App\Enums\Transactions\TransactionChannelEnum;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class WithdrawalRequest extends Model
+{
+    protected $fillable = [
+        "reference",
+        "account_id",
+        "amount_pesewas",
+        "narration",
+        "channel",
+        "momo_provider",
+        "wallet_number",
+        "idempotency_key",
+        "lien_id",
+        "maker_id",
+        "checker_id",
+        "status",
+        "decided_at",
+        "rejection_reason",
+        "ledger_reference",
+    ];
+
+
+    protected function casts(): array
+    {
+        return [
+            "amount_pesewas" => "integer",
+            "channel"=> TransactionChannelEnum::class,
+            "momo_provider"=> MomoProviderEnum::class,
+            // "status"=> WithdrawalRequestStatusEnum::class,
+            "decided_at" => "datetime"
+        ];
+    }
+
+
+    public function account(): BelongsTo
+    {
+       return $this->belongsTo(Account::class);
+    }
+
+
+    // public function lien(): BelongsTo
+    // {
+    //     return $this->belongsTo(Lien::class);
+    // }
+
+    // public function maker(): BelongsTo
+    // {
+    //     return $this->belongsTo(User::class, 'maker_id');
+    // }
+
+    // public function checker(): BelongsTo
+    // {
+    //     return $this->belongsTo(User::class, 'checker_id');
+    // }
+
+    // public function momoTransaction(): HasOne
+    // {
+    //     return $this->hasOne(MomoTransaction::class);
+    // }
+}
