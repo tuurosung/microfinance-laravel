7865a5a435b627106a0fadf63bf82653010851d5
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-07-03 03:40:02 +0000

    feat: Add allCifs method to CifRepositoryInterface for retrieving all CIF records

--- app/Domain/CIFs/Contracts/CifRepositoryInterface.php
+++ app/Domain/CIFs/Contracts/CifRepositoryInterface.php
@@ -3,11 +3,14 @@
 namespace App\Domain\CIFs\Contracts;
 
 use App\Domain\CIFs\Models\Cif;
+use Illuminate\Database\Eloquent\Collection;
 
 interface CifRepositoryInterface
 {
-
     public function create(array $data): Cif;
     public function update(Cif $cif, array $data): bool;
     public function delete(Cif $cif): bool;
+
+
+    public function allCifs(): Collection;
 }
