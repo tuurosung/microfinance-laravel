f7ded17fe1ba3084aa0a452235e05ac3dd4d22ff
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-07-08 04:31:13 +0000

    feat: Add new withdrawal modal component for transaction processing

---
+++ resources/views/components/transactions/⚡new-withdrawal.blade.php
@@ -0,0 +1,81 @@
+<?php
+
+use App\Domain\Accounts\Models\Account;
+use App\Enums\Transactions\TransactionChannelEnum;
+use Illuminate\Support\Str;
+use Livewire\Attributes\Computed;
+use Livewire\Component;
+
+new class extends Component
+{
+    public Account $account;
+
+    #[Computed]
+    public function idempotencyKey()
+    {
+        return Str::uuid();
+    }
+
+
+    #[Computed]
+    public function channel()
+    {
+        return TransactionChannelEnum::Counter;
+    }
+};
+?>
+
+
+<div>
+    <x-modals.modal modalId="new-withdrawal-modal">
+
+        <form method="POST" action="{{ route('transactions.withdrawal', $account) }}">
+            @csrf
+
+            <x-modals.modal-header modalId="new-deposit-modal" modalTitle="New Withdrawal" />
+
+            <div class="p-6">
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <input type="hidden" class="form-control" name="idempotency_key" id="idempotency_key" value="{{ $this->idempotencyKey }}" />
+                    </div>
+                </div>
+
+
+                <div class="row mb-8!">
+                    <div class="col-md-6">
+                        <label for="" class="form-label">Amount</label>
+                        <input type="number" class="form-control" name="amount" id="amount" required />
+                    </div>
+                </div>
+
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <label class="form-label">Channel</label>
+                        <select class="form-control" name="channel" id="channel">
+                            <option value="{{ $this->channel->value }}">{{ $this->channel->label() }}</option>
+                        </select>
+                    </div>
+                    <div class="col-md-6">
+                        <label class="form-label">Value Date</label>
+                        <input type="date" class="form-control" name="value_date" id="value_date" />
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <label for="" class="form-label">Narration</label>
+                        <textarea class="form-control resize-none" name="narration" id="narration"></textarea>
+                    </div>
+                </div>
+
+
+            </div>
+
+            <x-modals.modal-footer modalId="new-deposit-modal" btnLabel="Record Deposit" />
+
+        </form>
+    </x-modals.modal>
+</div>
