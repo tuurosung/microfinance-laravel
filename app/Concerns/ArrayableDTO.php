e6017de014557bdc49ac662cb39e74b3b82be6e0
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-07-09 02:28:22 +0000

    feat: Add ArrayableDTO trait for converting object properties to an associative array with snake_case keys

---
+++ app/Concerns/ArrayableDTO.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Concerns;
+
+use Illuminate\Support\Str;
+
+trait ArrayableDTO
+{
+    public function toArray(): array
+    {
+
+        $data = [];
+
+        foreach (get_object_vars($this) as $key => $value) {
+            $data[Str::snake($key)] = $value;
+        }
+
+        return $data;
+    }
+}
